7f8d77e9e5911d7669087ceaebc21769ef7737cf
Sarang <sarang1221@gmail.com>
2026-02-03 10:07:06 +0530

    Changes for application.name, removed underscores, causing issue with Load balancer, learned this the hardway

--- src/main/java/com/ecomm/ecomm_auth_service_application/controller/UserController.java
+++ src/main/java/com/ecomm/ecomm_auth_service_application/controller/UserController.java
@@ -3,6 +3,8 @@ package com.ecomm.ecomm_auth_service_application.controller;
 import com.ecomm.ecomm_auth_service_application.dto.UserDto;
 import com.ecomm.ecomm_auth_service_application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public UserDto getUserDetailsById(@PathVariable Long id) {
-        return userService.getUserDetailsById(id);
+    public ResponseEntity<UserDto> getUserDetailsById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUserDetailsById(id));
     }
 }
