8a5c70d68f727dfe2156942903d5d3b12c5adfd0
Sarang <sarang1221@gmail.com>
2026-07-14 19:22:25 +0530

    Removed User Controller and service, since profile service handles these, following SRP

--- src/main/java/com/ecomm/ecomm_auth_service_application/model/OtpType.java
+++
@@ -1,5 +0,0 @@
-package com.ecomm.ecomm_auth_service_application.model;
-
-public enum OtpType {
-    LOGIN_OTP, REGISTRATION_OTP, PASSWORD_RESET_OTP
-}
---
+++ src/main/java/com/ecomm/ecomm_auth_service_application/model/VerificationStatus.java
@@ -0,0 +1,4 @@
+package com.ecomm.ecomm_auth_service_application.model;
+
+public enum VerficationStatus {
+}
